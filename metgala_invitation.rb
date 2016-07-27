b2e1408d6f130c06ef312d7816d912a3d24fe610
nataliecostanza <costanzanatalie@gmail.com>
2016-07-27 04:31:51 +0000

    Done with invitations

--- metgala_invitation.rb
+++ metgala_invitation.rb
@@ -1,3 +1,8 @@
-#Code Here!
+#Met Gala Inivitation #Code Here!
+#Challenge 1
+gala_location = "The Museum of Modern Art"
+correct_gala_location = gala_location.gsub("The Museum of Modern Art", "The Metropolitan Museum")
+gala_date = "Sunday the 22nd of May 2015"
+correct_gala_date = gala_date.gsub("Sunday the 22nd of May 2015", "Sunday, May 13th 2017.")
 
-gala_invitation = "The Museum of Modern Art invites you to their annual gala on Sunday the 22nd of May 2015. Festivities will be held at the MoMA at 11 W 53rd St, New York, NY 10019. See you then!"
+puts gala_invitation = " #{correct_gala_location} invites you to their annual gala on #{correct_gala_date}. Festivities will be held at the MoMA at 11 W 53rd St, New York, NY 10019. See you then!"
